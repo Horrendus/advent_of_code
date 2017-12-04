76943a4ffcb4af853f50b7c31307202bb5223331
Stefan Derkits <stefan@derkits.at>
2017-12-04 19:43:11 +0100

    added: Day 4, Puzzle 2, changed: Day 4, Puzzle 1

--- src/day4_puzzle2.kt
+++ src/day4_puzzle2.kt
@@ -1,3 +1,5 @@
+
+
 fun main(args: Array<String>) {
     println("Enter puzzle input")
     val input = mutableListOf<String>()
@@ -17,10 +19,20 @@ private fun passphraseValid(passphrase: String): Boolean {
     for (i in 0 until words.size) {
         val word = words[i]
         for (j in i + 1 until words.size) {
-            if (word == words[j]) {
+            if (isAnagram(word, words[j])) {
+                // println("rejecting: $words")
                 return false
             }
         }
     }
+    // println("accepting: $words")
     return true
+}
+
+// function returns true if the word are anagrams
+private fun isAnagram(word1: String, word2: String): Boolean {
+    val isAnagram = word1.all {
+        word2.contains(it)
+    } && word1.length == word2.length
+    return isAnagram
 }
