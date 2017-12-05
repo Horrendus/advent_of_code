111754975fc50c973d58d390da678bfe71a9b434
Stefan Derkits <stefan@derkits.at>
2017-12-05 12:51:14 +0100

    added: Day 5, Puzzle 1 & 2

---
+++ src/day5_puzzle1.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("Enter puzzle input")
+    val input = mutableListOf<Int>()
+    while (true) {
+        val line = readLine() ?: break
+        input.add(Integer.parseInt(line))
+    }
+    var jumps = 0
+    var currentPosition = 0
+    while (currentPosition < input.size && currentPosition >= 0) {
+        val jump = input[currentPosition]
+        input[currentPosition]++
+        currentPosition += jump
+        jumps++
+    }
+    print("Finished after $jumps Jumps")
+}
