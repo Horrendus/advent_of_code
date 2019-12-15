51c49670b0e8afd396cec6d40da57fc72da72011
Stefan Derkits <stefan@derkits.at>
2019-12-15 21:54:08 +0100

    2019: day9 finished

--- 2019/2019_day9.py
+++ 2019/2019_day9.py
@@ -14,8 +14,15 @@ async def puzzle1(program):
     await computer.run()
 
 
-def puzzle2(data):
-    pass
+async def puzzle2(program):
+    async def simple_input():
+        return 2
+
+    async def simple_output(operand):
+        print("Output: ", operand)
+
+    computer = intcode.IntCodeComputer(program, simple_input, simple_output)
+    await computer.run()
 
 
 def main():
@@ -23,6 +30,7 @@ def main():
         data = f.read()
     program = [int(s.strip()) for s in data.split(",")]
     asyncio.run(puzzle1(program))
+    asyncio.run(puzzle2(program))
 
 
 if __name__ == "__main__":
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -37,119 +37,95 @@ class IntCodeComputer:
         return self.program[address]
 
     async def input(self, opcode_list):
-        operand1 = self.read_memory(self.sp + 1)
-        # operand1 = (
-        #     operand1
-        #     if opcode_list[2] == 1
-        #     else self.relative_base + operand1
-        #     if opcode_list[2] == 1
-        #     else self.read_memory(operand1)
-        # )
+        operand1 = self.get_address(1, opcode_list)
         res = await self.input_function()
         self.write_memory(res, operand1)
         self.sp += 2
 
     async def output(self, opcode_list):
-        operand1 = self.get_one_operand(opcode_list)
+        operand1 = self.get_value(1, opcode_list)
         await self.output_function(operand1)
         self.sp += 2
 
     async def add(self, opcode_list):
-        operand1, operand2, operand3 = self.get_three_operands(opcode_list)
+        operand1, operand2, operand3 = (
+            self.get_value(1, opcode_list),
+            self.get_value(2, opcode_list),
+            self.get_address(3, opcode_list),
+        )
         res = operand1 + operand2
         self.write_memory(res, operand3)
         self.sp += 4
 
     async def multiply(self, opcode_list):
-        operand1, operand2, operand3 = self.get_three_operands(opcode_list)
+        operand1, operand2, operand3 = (
+            self.get_value(1, opcode_list),
+            self.get_value(2, opcode_list),
+            self.get_address(3, opcode_list),
+        )
         res = operand1 * operand2
         self.write_memory(res, operand3)
         self.sp += 4
 
     async def jmp_nzero(self, opcode_list):
-        operand1, operand2 = self.get_two_operands(opcode_list)
+        operand1, operand2 = (
+            self.get_value(1, opcode_list),
+            self.get_value(2, opcode_list),
+        )
         if operand1 != 0:
             self.sp = operand2
         else:
             self.sp += 3
 
     async def jmp_zero(self, opcode_list):
-        operand1, operand2 = self.get_two_operands(opcode_list)
+        operand1, operand2 = (
+            self.get_value(1, opcode_list),
+            self.get_value(2, opcode_list),
+        )
         if operand1 == 0:
             self.sp = operand2
         else:
             self.sp += 3
 
     async def lt_other(self, opcode_list):
-        operand1, operand2, operand3 = self.get_three_operands(opcode_list)
+        operand1, operand2, operand3 = (
+            self.get_value(1, opcode_list),
+            self.get_value(2, opcode_list),
+            self.get_address(3, opcode_list),
+        )
         res = operand1 < operand2
         self.write_memory(int(res), operand3)
         self.sp += 4
 
     async def eq_other(self, opcode_list):
-        operand1, operand2, operand3 = self.get_three_operands(opcode_list)
+        operand1, operand2, operand3 = (
+            self.get_value(1, opcode_list),
+            self.get_value(2, opcode_list),
+            self.get_address(3, opcode_list),
+        )
         res = operand1 == operand2
         self.write_memory(int(res), operand3)
         self.sp += 4
 
     async def adj_relative_base(self, opcode_list):
-        operand1 = self.get_one_operand(opcode_list)
+        operand1 = self.get_value(1, opcode_list)
         self.relative_base += operand1
         self.sp += 2
 
-    def get_one_operand(self, opcode_list):
-        param_mode_1 = opcode_list[2]
-        operand1 = self.read_memory(self.sp + 1)
-        operand1, _, _ = self.parameter_translation(operand1, 0, 0, param_mode_1, 0, 0)
-        return operand1
+    def get_value(self, position, opcode_list):
+        address = self.get_address(position, opcode_list)
+        return self.read_memory(address)
 
-    def get_two_operands(self, opcode_list):
-        param_mode_1 = opcode_list[2]
-        param_mode_2 = opcode_list[1]
-        operand1 = self.read_memory(self.sp + 1)
-        operand2 = self.read_memory(self.sp + 2)
-        op1, op2, _ = self.parameter_translation(
-            operand1, operand2, 0, param_mode_1, param_mode_2, 0
-        )
-        return op1, op2
-
-    def get_three_operands(self, opcode_list):
-        param_mode_1 = opcode_list[2]
-        param_mode_2 = opcode_list[1]
-        param_mode_3 = opcode_list[0]
-        operand1 = self.read_memory(self.sp + 1)
-        operand2 = self.read_memory(self.sp + 2)
-        operand3 = self.read_memory(self.sp + 3)
-        operand1, operand2, operand3 = self.parameter_translation(
-            operand1, operand2, operand3, param_mode_1, param_mode_2, param_mode_3
-        )
-        return operand1, operand2, operand3
-
-    def parameter_translation(
-        self, operand1, operand2, operand3, param_mode_1, param_mode_2, param_mode_3
-    ) -> Tuple[int, int]:
-        op1 = (
-            operand1
-            if param_mode_1 == 1
-            else self.read_memory(self.relative_base + operand1)
-            if param_mode_2 == 2
-            else self.read_memory(operand1)
-        )
-        op2 = (
-            operand2
-            if param_mode_2 == 1
-            else self.read_memory(self.relative_base + operand2)
-            if param_mode_2 == 2
-            else self.read_memory(operand2)
-        )
-        # op3 = (
-        #     operand3
-        #     if param_mode_3 == 1
-        #     else self.relative_base + operand3
-        #     if param_mode_3 == 2
-        #     else self.read_memory(operand3)
-        # )
-        return op1, op2, operand3
+    def get_address(self, position, opcode_list):
+        val = self.read_memory(self.sp + position)
+        param_mode = opcode_list[3 - position]
+        if param_mode == 1:
+            return self.sp + position
+        elif param_mode == 2:
+            return self.relative_base + val
+        elif param_mode == 0:
+            return val
+        assert False
 
     @staticmethod
     def parse_opcode(opcode):
