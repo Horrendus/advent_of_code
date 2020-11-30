2af0fc49bc3a48da5fa4dde2f530ad283c3c212c
Stefan Derkits <stefan@derkits.at>
2020-11-30 20:51:48 +0000

    2020: add template

---
+++ 2020/2020_template.py
@@ -0,0 +1,21 @@
+def puzzle1(data):
+    pass
+
+def puzzle2(data):
+    pass
+
+
+def parse_input(data):
+    return data
+
+
+def main():
+    with open("input/input_day1.txt") as f:
+        data = f.read()
+    data = parse_input(data)
+    puzzle1(data)
+    puzzle2(data)
+
+
+if __name__ == "__main__":
+    main()
